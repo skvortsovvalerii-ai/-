dd2a26770b2efc5bb4b12ba6d3093c5f751a4618
valera <valera.skvortsov@chnu.edu.ua>
2026-05-22 01:11:03 +0300

    task 6

---
+++ task 6.py
@@ -0,0 +1,8 @@
+keywords = ('for', 'if', 'else', 'in', ':')
+indent = '    '  # 4 пробіли = 1 відступ
+
+print(keywords[0] + " each token " + keywords[3] + " the postfix expression " + keywords[4])
+print(indent + keywords[1] + " the token is a number " + keywords[4])
+print(indent + indent + "print('Convert it to an integer and add it to the end of values')")
+print(indent + keywords[2])
+print(indent + indent + "print('Append the result to the end of values')")

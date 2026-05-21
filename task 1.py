01ca973b4ed63d9e45599e3a186b6936b1945c88
valera <valera.skvortsov@chnu.edu.ua>
2026-05-21 23:36:08 +0300

    task 1

---
+++ task 1.py
@@ -0,0 +1,14 @@
+languages = ['Ukrainian', 'French', 'Bulgarian', 'Norwegian', 'Latvian', 'Japanese', 'Arabic']
+
+print(languages)
+
+print("Sorted:")
+print(sorted(languages))
+
+languages.reverse()
+print("Reversed:")
+print(languages)
+
+languages.sort()
+print("Sort:")
+print(languages)
